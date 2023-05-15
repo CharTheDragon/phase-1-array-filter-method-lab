6e170060d8665601ad4a7bb40f762afbb51522e6
Nathaniel Swan <cygnet1.999@gmail.com>
2023-05-15 11:58:44 -0400

    that took too long

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(array, personWeAreLookingFor) {
+    return array.filter(name => name.toUpperCase() === personWeAreLookingFor.toUpperCase())
+}
+
+function fuzzyMatch(array, firstLetterOfPersonWeAreLookingFor) {
+    return array.filter(name => name.slice(0,1) === firstLetterOfPersonWeAreLookingFor.slice(0,1))
+}
+
+function matchName(array, personWeAreLookingFor) {
+    return array.filter(name => name.name === personWeAreLookingFor)
+}
